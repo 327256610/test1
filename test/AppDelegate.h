15c311d83e1e279740717ab5097c97df9cb369e6
陈建欢 <chenjianhuan@chenjianhuandeiMac.local>
2017-02-27 18:30:00 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 陈建欢 on 2017/2/27.
+//  Copyright © 2017年 陈建欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 陈建欢 on 2017/2/27.
+//  Copyright © 2017年 陈建欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
