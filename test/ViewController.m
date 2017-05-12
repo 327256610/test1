ac75990038881cf9ea148e4b57cb518576180bc7
陈建欢 <chenjianhuan@chenjianhuandeiMac.local>
2017-05-12 10:24:43 +0800

    1

--- test/ViewController.m
+++ test/ViewController.m
@@ -16,10 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    
+    
+    
+    
+    
 }
 
+/**
+ <#Description#>
 
+ @param sender <#sender description#>
+ */
+- (IBAction)test:(id)sender {
+    UIView * view = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
+    [UIView animateWithDuration:3 animations:^{
+        view.frame = CGRectMake(400, 400, 200, 200);
+    } completion:^(BOOL finished) {
+        view.alpha = 0.5;
+    }];
+  
+    
+}
+
+/**
+ <#Description#>
+ */
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
